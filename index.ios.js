cf1bc7bc7196849c5df7a2afa48aa509fe037b3d
Charles Julian Knight <julian@fixdapp.com>
2016-12-10 20:09:25 -0500

    created FriendCard design

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+
+import FriendCard from './shared/FriendCard'
 
 export default class FriendRM extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       To get started, edit index.ios.js
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       Press Cmd+R to reload,{'\n'}
+    //       Cmd+D or shake for dev menu
+    //     </Text>
+    //   </View>
+    // );
+    const opts = {
+      width: 100,
+      // avatar: '💩',
+      avatar: 'https://www.gravatar.com/avatar/e3f99640d60577f72086b54087423593.png?s=200',
+      lastContacted: new Date(new Date().getTime() - 3*7*24*60*60*1000),
+      // name: 'Nikolaos Papanikolopoulos'
+      name: 'John Smith'
+    }
+    return (<FriendCard {...opts} style={{margin: 40}} />)
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
 
-AppRegistry.registerComponent('FriendRM', () => FriendRM);
+AppRegistry.registerComponent('FriendRM', () => FriendRM)
--- shared/FriendCard.js
+++ shared/FriendCard.js
@@ -1,22 +1,82 @@
 import React, { Component, PropTypes } from 'react'
+import { View, Text, Image, ActivityIndicator } from 'react-native'
+import moment from 'moment'
 
 class FriendCard extends Component {
 
-  static propTypes = {
-    lastContacted: PropTypes.instanceOf(Date),
-    avatar: (props, propName, componentName) => {
-      const val = props[propName]
-      if(!val || !(
-        val.match(/^https?:\/\/.*/)
-        || val.match(/^[\uD83C-\uDBFF\uDC00-\uDFFF]{2}$/)
-      )){
-        throw `Invalid avatar (must be url or emoji): ${val}`
-      }
-    }
-  }
-
   render() {
+    const radius = this.props.style.borderRadius || 5;
+    return (
+      <View style={{
+        height: (this.props.width * 1.56),
+        width: this.props.width,
+        shadowColor: 'black',
+        shadowOffset: {width:0, height: 7},
+        shadowOpacity: .5,
+        shadowRadius: 9,
+        borderRadius: radius,
+        ...this.props.style
+      }}>
+        <View style={{
+          flex: 2,
+          backgroundColor: '#FF4040',
+          borderTopLeftRadius: radius,
+          borderTopRightRadius: radius,
+          flexDirection: 'row'
+        }}>
+          <Text style={{flex: 1, textAlign: 'center', alignSelf: 'center', fontFamily: 'Avenir'}}>
+            {moment(this.props.lastContacted).fromNow()}
+          </Text>
+        </View>
+        <View style={{
+            height: this.props.width,
+            flexDirection: 'row',
+            padding: (0.11*this.props.width)
+          }}>{
+          this.props.avatar.length == 2
+          ? <Text style={{
+              flex: 1,
+              fontSize: 30,
+              alignSelf: 'center',
+              textAlign: 'center'
+            }}>{this.props.avatar}</Text>
+          : <Image style={{flex: 1, borderRadius: 6}} source={{uri: this.props.avatar}} />
+        }</View>
+        <View style={{
+            flex: 3,
+            flexDirection: 'row',
+            borderBottomLeftRadius: radius,
+            borderBottomRightRadius: radius
+          }}>
+          <Text 
+            adjustsFontSizeToFit={true}
+            minimumFontScale={0.4}
+            numberOfLines={2}
+            ellipsizeMode='tail'
+            style={{
+              flex: 1,
+              marginBottom: radius,
+              textAlign: 'center',
+              fontFamily: 'Avenir',
+              fontSize: 16
+            }}>{this.props.name}</Text>
+        </View>
+      </View>
+    )
+  }
+}
 
+FriendCard.propTypes = {
+  width: PropTypes.number,
+  lastContacted: PropTypes.instanceOf(Date),
+  avatar: (props, propName, componentName) => {
+    const val = props[propName]
+    if(!val || !(
+      val.match(/^https?:\/\/.*/)
+      || val.match(/^[\uD83C-\uDBFF\uDC00-\uDFFF]{2}$/)
+    )){
+      return Error(`Invalid avatar (must be url or emoji): ${val}`)
+    }
   }
 }
 
