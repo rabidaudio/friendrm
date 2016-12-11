06931b5896f2fe0c25544c44f93043b2bc3a1aab
Charles Julian Knight <julian@fixdapp.com>
2016-12-11 04:54:56 -0500

    added the second dimension

--- index.ios.js
+++ index.ios.js
@@ -1,41 +1,75 @@
 // @flow
 import React, { Component } from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, ScrollView } from 'react-native'
 
 import FriendGroup from './shared/FriendGroup'
 
 import Friend from './lib/models/Friend'
 import Rule from './lib/models/Rule'
 import Contact from './lib/models/Contact'
+import type {State} from './lib/models/State'
 
-export default class FriendRM extends Component {
+const textEveryTwoWeeks = new Rule({id: 1, channel: 'text', frequency: 14})
 
-  render () {
-    const john = new Friend({
-      name: 'John Smith',
-      avatar: 'https://www.gravatar.com/avatar/e3f99640d60577f72086b54087423593.png?s=200',
-      contacts: []
-    })
-    john.contacts.push(new Contact({
+const contactEverySixMonths = new Rule({id: 2, channel: null, frequency: 6 * 30})
+
+const john = new Friend({
+  id: 1,
+  name: 'John Smith',
+  avatar: 'https://www.gravatar.com/avatar/e3f99640d60577f72086b54087423593.png?s=200',
+  contacts: [
+    new Contact({
       timestamp: new Date(2016, 11, 8),
       // timestamp: new Date(2016, 10, 8),
       channel: 'text',
       friend: john
-    }))
-    john.contacts.push(new Contact({
+    }),
+    new Contact({
       timestamp: new Date(2015, 11, 25),
       channel: 'call',
       friend: john
-    }))
-    const niko = new Friend({
-      name: 'Nikolaos Papanikolopoulos',
-      avatar: '💩',
-      contacts: []
     })
-    return (<FriendGroup
-      rule={new Rule({channel: 'text', frequency: 14})}
-      friends={[john, niko]}
-      style={{marginTop: 40}} />)
+  ]
+})
+const niko = new Friend({
+  id: 2,
+  name: 'Nikolaos Papanikolopoulos',
+  avatar: '💩',
+  contacts: []
+})
+
+const state: State = {
+  rules: [textEveryTwoWeeks, contactEverySixMonths],
+  friends: {[john.id]: john, [niko.id]: niko},
+  rules_friends: [
+    [textEveryTwoWeeks.id, john.id],
+    [textEveryTwoWeeks.id, niko.id],
+    [contactEverySixMonths.id, john.id]
+  ]
+}
+
+function getFriendsForRule (rule: Rule): Friend[] {
+  return state.rules_friends.filter((a) => {
+    const [ruleId] = a
+    return rule.id === ruleId
+  }).map((a) => {
+    const [ , friendId ] = a
+    return state.friends[friendId]
+  })
+}
+
+export default class FriendRM extends Component {
+
+  render () {
+    const groups = state.rules.map((rule) => {
+      const friendsForRule = getFriendsForRule(rule)
+      return (<FriendGroup
+        key={rule.id}
+        rule={rule}
+        friends={friendsForRule}
+        style={{marginTop: 40}} />)
+    })
+    return (<ScrollView>{groups}</ScrollView>)
   }
 }
 
